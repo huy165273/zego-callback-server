03ff1c2e2c76ee189ad4ad36981bf8f84862f142
kiencoam <kienpham1392004@gmail.com>
2025-11-24 16:37:12 +0700

    extend limit to 100mb

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 3000;
 const MONGODB_URI = 'mongodb+srv://kienpham1392004:1392004kien@cluster0.byk8u.mongodb.net/audio-moderation-results?retryWrites=true&w=majority&appName=Cluster0';
 
 // Middleware
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 // Connect to MongoDB
 mongoose.connect(MONGODB_URI)
